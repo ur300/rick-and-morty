1dfc65963b988a8cbfc77593a3a14e84dd16da9c
Pavlo Vinnyk <3381873+ur300@users.noreply.github.com>
2025-09-28 18:05:42 +0200

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/rick-and-morty/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   plugins: [
     tanstackRouter({ autoCodeSplitting: true }),
     tailwindcss(),
